43148781d660166b7777e8ef1829afa62572e17b
Şahin Ataman <shn.ataman@gmail.com>
2017-06-10 16:35:46 +0300

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace MultiFaceRec
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBoxFrameGrabber = new Emgu.CV.UI.ImageBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxFrameGrabber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Location = new System.Drawing.Point(81, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "2. Yüz Ekle";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 151);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(88, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.imageBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(368, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(184, 242);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Çalışma Alanı: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "İsim-Soyisim: ";
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox1.Location = new System.Drawing.Point(25, 19);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(134, 103);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.imageBox1.TabIndex = 5;
+            this.imageBox1.TabStop = false;
+            this.imageBox1.Click += new System.EventHandler(this.imageBox1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(592, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(209, 242);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sonuçlar: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(9, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 15);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Kameradaki kişiler : ";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(9, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 19);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Hiçkimse";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(163, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 15);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Algılanan yüz sayısı :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(86, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "1. Yüz algılamak için tıklayınız...";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBoxFrameGrabber
+            // 
+            this.imageBoxFrameGrabber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBoxFrameGrabber.Location = new System.Drawing.Point(12, 14);
+            this.imageBoxFrameGrabber.Name = "imageBoxFrameGrabber";
+            this.imageBoxFrameGrabber.Size = new System.Drawing.Size(319, 240);
+            this.imageBoxFrameGrabber.TabIndex = 4;
+            this.imageBoxFrameGrabber.TabStop = false;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(813, 300);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.imageBoxFrameGrabber);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmPrincipal";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Yüz Tanıma  ve Algılama";
+            this.Load += new System.EventHandler(this.FrmPrincipal_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxFrameGrabber)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private Emgu.CV.UI.ImageBox imageBoxFrameGrabber;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,220 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using System.IO;
+using System.Diagnostics;
+
+namespace MultiFaceRec
+{
+    public partial class FrmPrincipal : Form
+    {
+        //Tüm değişkenleri tanımlama : vectors and haarcascades
+        Image<Bgr, Byte> currentFrame;
+        Capture grabber;
+        HaarCascade face;
+        HaarCascade eye;
+        MCvFont font = new MCvFont(FONT.CV_FONT_HERSHEY_TRIPLEX, 0.5d, 0.5d);
+        Image<Gray, byte> result, TrainedFace = null;
+        Image<Gray, byte> gray = null;
+        List<Image<Gray, byte>> trainingImages = new List<Image<Gray, byte>>();
+        List<string> labels= new List<string>();
+        List<string> NamePersons = new List<string>();
+        int ContTrain, NumLabels, t;
+        string name, names = null;
+
+
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+            //Yüz algılamaları için haarcascades yükle
+            face = new HaarCascade("haarcascade_frontalface_default.xml");
+            //eye = new HaarCascade("haarcascade_eye.xml");
+            try
+            {
+                //Her resim için önceden kayıtlı yüzlerin ve etiketlerin yüklenmesi
+                string Labelsinfo = File.ReadAllText(Application.StartupPath + "/TrainedFaces/KayitliKisiler.txt");
+                string[] Labels = Labelsinfo.Split('%');
+                NumLabels = Convert.ToInt16(Labels[0]);
+                ContTrain = NumLabels;
+                string LoadFaces;
+
+                for (int tf = 1; tf < NumLabels+1; tf++)
+                {
+                    LoadFaces = "face" + tf + ".bmp";
+                    trainingImages.Add(new Image<Gray, byte>(Application.StartupPath + "/TrainedFaces/" + LoadFaces));
+                    labels.Add(Labels[tf]);
+                }
+            
+            }
+            catch(Exception e)
+            {
+                //Yüz tanıma için bilgilendirme mesajı ekrana yazdırma
+                MessageBox.Show("Veritabanında kayıtlı hiçbir FACE bulunmamaktadır.Lütfen bir FACE ekleyiniz", "DİKKAT", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+        }
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imageBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Cameranın çalıştırılması
+            grabber = new Capture();
+            grabber.QueryFrame();
+            //Camera çerceve olayının çalıştırılması
+            Application.Idle += new EventHandler(FrameGrabber);
+            button1.Enabled = false;
+        }
+
+
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                //Eklenecek alandaki FACE sayacı
+                ContTrain = ContTrain + 1;
+
+                //Camera da  Gri cerceve aliyoruz
+                gray = grabber.QueryGrayFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+
+                //Yüz tanımaa
+                MCvAvgComp[][] facesDetected = gray.DetectHaarCascade(
+                face,
+                1.2,
+                10,
+                Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+                new Size(20, 20));
+
+                //Algılanan her cerceve için yapılacak işlemler
+                foreach (MCvAvgComp f in facesDetected[0])
+                {
+                    TrainedFace = currentFrame.Copy(f.rect).Convert<Gray, byte>();
+                    break;
+                }
+
+                //Tespit edilen cercevenin onceki kayıtlı cercevelerle karşılaştırılması
+                TrainedFace = result.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                trainingImages.Add(TrainedFace);
+                labels.Add(textBox1.Text);
+
+                //Yüzü gri tonlama ile gösterme
+                imageBox1.Image = TrainedFace;
+
+                File.WriteAllText(Application.StartupPath + "/TrainedFaces/KayitliKisiler.txt", trainingImages.ToArray().Length.ToString() + "%");
+
+                //Cercevelerin karşılaştırılma kısmı
+                for (int i = 1; i < trainingImages.ToArray().Length + 1; i++)
+                {
+                    trainingImages.ToArray()[i - 1].Save(Application.StartupPath + "/TrainedFaces/face" + i + ".bmp");
+                    File.AppendAllText(Application.StartupPath + "/TrainedFaces/KayitliKisiler.txt", labels.ToArray()[i - 1] + "%");
+                }
+
+                MessageBox.Show(textBox1.Text + "´nın yüzü algılandı ve kaydedildi :)", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Önce yüz algılamayı etkinleştir", " Hata", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+
+        void FrameGrabber(object sender, EventArgs e)
+        {
+            label3.Text = "0";
+            //label4.Text = "";
+            NamePersons.Add("");
+
+
+            //O anki camera boyutları
+            currentFrame = grabber.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+
+            //Gri ye çevirme
+            gray = currentFrame.Convert<Gray, Byte>();
+
+            //Yüz Tanıma
+            MCvAvgComp[][] facesDetected = gray.DetectHaarCascade(
+                 face,
+                 1.2,
+                 10,
+                 Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+                 new Size(50, 50));
+
+            //Algılanan her öğe için yapılacak işlemler
+            foreach (MCvAvgComp f in facesDetected[0])
+            {
+                t = t + 1;
+                result = currentFrame.Copy(f.rect).Convert<Gray, byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                //Tespit edilen yüzün kırmızı kare içerisine alınması 
+                currentFrame.Draw(f.rect, new Bgr(Color.Red), 2);
+
+                if (trainingImages.ToArray().Length != 0)
+                {
+                    //Yüz Tanıma için TermCriteria yaratma
+                    MCvTermCriteria termCrit = new MCvTermCriteria(ContTrain, 0.001);
+                    //Yüz tanıyıcısı
+                    FaceRecognizer recognizer = new FaceRecognizer(
+                           trainingImages.ToArray(),
+                           labels.ToArray(),
+                           3000,
+                           ref termCrit);
+                    name = recognizer.Recognize(result);
+                    //Cercevenin üzerindeki yazı (Kişi Tanıyıcı)
+                    currentFrame.Draw(name, ref font, new Point(f.rect.X -5,f.rect.Y - 5), new Bgr(Color.LightGreen));
+
+                }
+
+                NamePersons[t - 1] = name;
+                NamePersons.Add("");
+
+
+                //Camera da algılanan yüz sayısı
+                label3.Text = facesDetected[0].Length.ToString();
+
+            }
+            t = 0;
+
+            //Tanımlanan kişilerin isimlerinin birleştirilmesi
+            for (int nnn = 0; nnn < facesDetected[0].Length; nnn++)
+            {
+                names = names + NamePersons[nnn] + ", ";
+            }
+            //Tanınan Yüzü gösterme
+            imageBoxFrameGrabber.Image = currentFrame;
+            label4.Text = names;
+            names = "";
+            //İsim listesini temizleme
+            NamePersons.Clear();
+
+        }
+
+    }
+}
